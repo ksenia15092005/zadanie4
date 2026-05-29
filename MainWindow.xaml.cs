a8dc2e091b7131db18f7a308892c17cfcd8d780e
Kozlova Ksenia <ksenia15092005431166@gmail.com>
2026-05-29 14:19:32 +0300

    Коммит в дополнительной ветке

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -81,6 +81,7 @@ namespace zadanie4
 
                     string fio = "Козлова Ксения Максимовна ";
                     // Это коммит в ветке feature-branch
+                    int test = 1;
 
                     string result = $"ФИО: {fio}\n\n";
                     result += $"Частота дискретизации: {sampleRate} Гц\n";
